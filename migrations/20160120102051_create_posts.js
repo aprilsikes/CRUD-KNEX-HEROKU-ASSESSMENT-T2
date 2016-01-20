a8a4d1a1cfe0156b7e55cb181f8db9115fb81235
april sikes <aprilsikes@gmail.com>
2016-01-20 10:34:43 -0700

    initial commit

---
+++ migrations/20160120102051_create_posts.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('posts', function (table) {
+  table.increments();
+  table.string('author');
+  table.string('body');
+  table.timestamps();
+});
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('posts');
+};
