309fba98517c3d8431befd0932d93041ac264176
april sikes <aprilsikes@gmail.com>
2016-01-20 11:11:23 -0700

    3 tests passing

--- routes/posts.js
+++ routes/posts.js
@@ -6,11 +6,32 @@ function Posts() {
   return knex('posts');
 }
 
-
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   Posts().select().then(function (posts) {
     res.json({'SUCCESS': posts });
-  })
+  });
 });
+
+router.post('/', function (req, res, next) {
+
+  var thePost = {
+    author:req.params.author,
+    body:req.params.body
+  }
+
+  Posts().insert(thePost).then(function (posts) {
+    res.redirect('/');
+  });
+});
+
+router.get('/:id', function (req, res, next) {
+  Posts().where('id', (req.params.id)).first().then(function (posts) {
+    res.json({'SUCCESS': posts});
+  });
+});
+
+router.get('/:id/edit', function (req, res, next) {
+  
+})
 
 module.exports = router;
